5ada8f2db3d36710cf35e3e4ed0771eef979be7c
Ali Nasser <alinasserelden@hotmail.com>
2022-12-14 14:43:01 +0200

    required the db files

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const authRoute = require("./controllers/authentication");
+const authRoute = require("./controllers/authentication")
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 const express = require('express')
 const bcrypt = require('bcrypt')
 const dotenv = require('dotenv')
+require('./database/database')
 const cors = require('cors')
 const app = express()
 dotenv.config()
+
+
+//requering DB files
+const User = require('./models/User')
+const Cart = require('./models/Cart')
+const Paintings = require('./models/Painting')
+const purchases = require('./models/Purchase')
 
 
 app.use(express.json())
